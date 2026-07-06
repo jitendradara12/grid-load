85d6ab831d8d6b5c6f3e18e8aca7803eddf89429
sastauser <jitendradara4464@gmail.com>
2026-07-06 21:07:59 +0530

    fix: skipping merge when npp_df is empty

--- src/data_pipeline.py
+++ src/data_pipeline.py
@@ -10,21 +10,27 @@ if iced_df.empty:
     print(f"Put ICED dataset in path: {ICED_PATH}\n")
     exit()
 
+
 if npp_df.empty:
     print(f"WARNING: NPP dataset is not available at {NPP_PATH}\n")
     print("Skipping merge...\n")
+    npp_flag = False
 else:
+    npp_flag = True
+
+if npp_flag:
     npp_df["datetime"] = pd.to_datetime(npp_df["datetime"])
-    iced_df["datetime"] = pd.to_datetime(iced_df["datetime"])
-
     npp_df = npp_df.resample("h", on="datetime")["value"].mean().reset_index()
+iced_df["datetime"] = pd.to_datetime(iced_df["datetime"])
 
-    # npp_df.set_index('datetime', inplace=True)
-    # iced_df.set_index('datetime', inplace=True)
 
-    SPLIT_TIME = "2025-12-30 12:00:00"
+# npp_df.set_index('datetime', inplace=True)
+# iced_df.set_index('datetime', inplace=True)
 
-    iced_df_filtered = iced_df[iced_df["datetime"] < SPLIT_TIME]
+SPLIT_TIME = "2025-12-30 12:00:00"
+
+iced_df_filtered = iced_df[iced_df["datetime"] < SPLIT_TIME]
+if npp_flag:
     npp_df_filtered = npp_df[npp_df["datetime"] >= SPLIT_TIME]
 
     combined = (
@@ -32,13 +38,16 @@ else:
         .sort_values("datetime")
         .reset_index(drop=True)
     )
+else:
+    combined = iced_df_filtered
 
-    combined["value"] = combined["value"].interpolate(method="linear").ffill().bfill()
 
-    print(
-        f"Combined time series spans from {combined['datetime'].min()} to {combined['datetime'].max()}."
-    )
-    print(f"Total hourly rows: {len(combined)}")
+combined["value"] = combined["value"].interpolate(method="linear").ffill().bfill()
+
+print(
+    f"Combined time series spans from {combined['datetime'].min()} to {combined['datetime'].max()}."
+)
+print(f"Total hourly rows: {len(combined)}")
 
 
 def get_train_dataframe():
